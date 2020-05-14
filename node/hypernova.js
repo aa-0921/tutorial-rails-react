94b64ff05bf09bea23ab09b27de871f1f83b781a
akuto aoyama <[akutotuka@gmail.com]>
2020-05-14 10:55:28 +0900

    application.html.erb修正

--- node/hypernova.js
+++ node/hypernova.js
@@ -1,10 +1,11 @@
-import hypernova from 'hypernova/server'
+import hypernova from 'hypernova/server';
 import * as exposed from '../app/javascript/exposeComponents';
 
 hypernova({
   devMode: process.env.NODE_ENV !== 'production',
-  getComponen(name) {
+  getComponent(name) {
     return exposed[name]
   },
   port: 3001
 });
+
